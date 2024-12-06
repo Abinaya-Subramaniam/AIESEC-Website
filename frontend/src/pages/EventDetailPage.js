a430e9a7e39eaa32f9eb40103113b224b9751842
Abinaya <abi12subramaniam@gmail.com>
2024-12-06 20:55:17 +0530

    After Movie Links

--- frontend/src/pages/EventDetailPage.js
+++ frontend/src/pages/EventDetailPage.js
@@ -34,37 +34,43 @@ const EventDetailPage = () => {
       images: [leadcs1, leadcs2, leadcs3, leadcs4],
       description:
         'LEAD CS is the annual Leadership Development Seminar organized by AIESEC in University of Moratuwa. This event facilitates our new members with fundamental leadership skills through proper coaching and team-building sessions along with professional educators and facilitators certified in leadership training.',
+        videoUrl: 'https://www.youtube.com/embed/JuIgZZBIm0s',
     },
     2: {
       title: 'Global Village',
       images: [gv1, gv2, gv3, gv4],
       description:
         'Global Village, organized by AIESEC in University of Moratuwa, is the cultural festival of foreign Exchange Participants from all over the world and AIESEC in 19 universities all across the country which will be held at the University of Moratuwa premises. This brings youth from diverse cultural backgrounds together to celebrate multi-ethnicity, cross-culture and global citizenship.',
+        videoUrl: 'https://www.youtube.com/embed/tPsRuAU2CDY',
     },
     3: {
       title: 'IDEALIZE',
       images: [idealize1, idealize2, idealize3, idealize4],
       description:
         'Idealize 2024 is a Mobile App and Web development competition beyond an ordinary ideathon organized by AIESEC in University of Moratuwa for school students and undergraduates islandwide...',
-    },
+        videoUrl: 'https://www.youtube.com/embed/LLq2C7A8gfM',
+      },
     4: {
       title: 'Hope',
       images: [hope1, hope2, hope3, hope4],
       description:
         'HOPE is a community development project which focuses on the activities aimed at uplifting those in need and creating a closer, more connected community...',
-    },
+        videoUrl: 'https://www.youtube.com/embed/OqkWciDonuQ',
+      },
     5: {
       title: 'CS Eminence',
       images: [cs1, cs2, cs3, cs4],
       description:
         'CS Eminence is the most glamorous and remarkable extravaganza within the AIESEC journey of the University of Moratuwa to appreciate the achievements of all the significant individuals...',
-    },
+        videoUrl: 'https://www.youtube.com/embed/hEbIO82zeEc',
+      },
     6: {
       title: 'Beauty Beyond Ceylon',
       images: [bbc1, bbc2, bbc3, bbc4],
       description:
         '"Beauty Beyond Ceylon 2.0" is an international event offering a unique chance to explore global cultures without leaving Sri Lanka...',
-    },
+        videoUrl: 'https://www.youtube.com/embed/FEC3qujCU1s',
+      },
   };
 
   const event = eventsData[eventId];
@@ -76,11 +82,27 @@ const EventDetailPage = () => {
   return (
     <div className="container mt-5">
       <h1 className="text-center mb-4">{event.title}</h1>
-      {/* Content-  Desccrbe */}
+      
+      {/* Content-  Description */}
       <div className="event-description mt-4" style={{ fontSize: '1.2rem', color: '#555' }}>
         <p>{event.description}</p>
       </div>
-      {/* Img */}
+
+      {/* YouTube Embed */}
+      <div className="event-video mt-4">
+        <h3 className="text-center">Watch the Aftermovie</h3>
+        <div className="embed-responsive embed-responsive-16by9">
+          <iframe
+            className="embed-responsive-item"
+            src={event.videoUrl}
+            title="Event Aftermovie"
+            allowFullScreen
+            style={{ border: 'none', width: '100%', height: '400px' }}
+          ></iframe>
+        </div>
+      </div>
+
+      {/* Images */}
       <div className="row mt-4">
         <div className="col-md-12">
           <div className="d-flex flex-wrap justify-content-center">
